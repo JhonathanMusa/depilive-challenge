92a575ab599d1010a8fdff25144115a8723293d8
jhonathanmusa <jhonathansamu91@gmail.com>
2021-08-28 21:43:54 -0500

    the modal has been changed to load once when the user visit the page for the firstTime

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 let modal = document.getElementById("tvesModal");
 let btn = document.getElementsByClassName("close")[0];
-/* let span = document.getElementsByClassName("close")[0]; */
 let body = document.getElementsByTagName("body")[0];
 
-window.onload = function () {
-  modal.style.display = "block";
+if (!sessionStorage.getItem("user")) {
+  window.onload = function () {
+    modal.style.display = "block";
 
-  body.style.position = "static";
-  body.style.height = "100%";
-  body.style.overflow = "hidden";
-};
+    body.style.position = "static";
+    body.style.height = "100%";
+    body.style.overflow = "hidden";
+  };
 
-btn.onclick = function () {
-  modal.style.display = "none";
-
-  body.style.position = "inherit";
-  body.style.height = "auto";
-  body.style.overflow = "visible";
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
+  btn.onclick = function () {
     modal.style.display = "none";
 
     body.style.position = "inherit";
     body.style.height = "auto";
     body.style.overflow = "visible";
-  }
-};
+    sessionStorage.setItem("user", true);
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+
+      body.style.position = "inherit";
+      body.style.height = "auto";
+      body.style.overflow = "visible";
+    }
+  };
+}
